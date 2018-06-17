ef72fae29d468c4f7418c5f35280ec053bf3c851
ruofeng <lrf0829@126.com>
2018-06-17 15:14:49 +0800

    course arrangement

--- src/components/CourseArrangement/ViewCourse.vue
+++ src/components/CourseArrangement/ViewCourse.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
         <h1>排课结果</h1>
-        <Table size="large"  :columns="col" :data="data"></Table>
+        <Select v-model="selectType" style="width:120px">
+            <Option v-for="ty in typeList" :value="ty.value" :key="ty.value">{{ ty.label }}</Option>
+        </Select>
+        <Select v-model="selectedTeacher" style="width:100px">
+            <Option v-for="te in teacherList" :value="te.value" :key="te.value">{{ te.label }}</Option>
+        </Select>
+        <Select v-model="selectedCourse" style="width:150px">
+            <Option v-for="co in courseList" :value="co.value" :key="co.value">{{ co.label }}</Option>
+        </Select>
+        <Select v-model="selectedClass" style="width:100px">
+            <Option v-for="cl in classList" :value="cl.value" :key="cl.value">{{ cl.label }}</Option>
+        </Select>
+        <Button type="default" @click="fetchTableData">查看</Button>
+        <Table border :columns="col" :data="awesomeCourseList" :loading="isLoading"></Table>
         <br><br>
-        <Button type="primary">一键排课</Button>
+        <Button type="primary" @click="doArrange">一键排课</Button>
     </div>
 </template>
 <script>
@@ -14,7 +27,7 @@ export default {
                 {
                     title: "节数",
                     key: "class",
-                    width: 200,
+                    width: 120,
                     fixed: "left"
                 },
                 {
@@ -43,57 +56,165 @@ export default {
                     width: 200
                 }
             ],
-            data: [
+            course: Object,
+            awesomeCourseList: [
                 {
                     class: "1 (8:00-9:35)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    mon: "",
+                    two: "",
+                    wed: "",
+                    thu: "",
+                    fri: ""
                 },
                 {
                     class: "2 (9:55-11:30)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    mon: "",
+                    two: "",
+                    wed: "",
+                    thu: "",
+                    fri: ""
                 },
                 {
                     class: "3 (13:30-15:05)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    mon: "",
+                    two: "",
+                    wed: "",
+                    thu: "",
+                    fri: ""
                 },
                 {
                     class: "4 (15:20-16:55)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    mon: "",
+                    two: "",
+                    wed: "",
+                    thu: "",
+                    fri: ""
                 },
                 {
                     class: "5 (17:10-18:45)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    mon: "",
+                    two: "",
+                    wed: "",
+                    thu: "",
+                    fri: ""
+                }
+            ],
+            isLoading: false,
+            selectType: 0,
+            typeList: [
+                {
+                    value: 0,
+                    label: "按教师显示"
                 },
                 {
-                    class: "6 (19:30-21:05)",
-                    mon: "test",
-                    two: "test",
-                    wed: "test",
-                    thu: "test",
-                    fri: "test"
+                    value: 1,
+                    label: "按课程显示"
+                },
+                {
+                    value: 2,
+                    label: "按班级显示"
+                }
+            ],
+            selectedTeacher: 1,
+            teacherList: [
+                {
+                    value: 1,
+                    label: "老师A"
+                },
+                {
+                    value: 2,
+                    label: "老师B"
+                }
+            ],
+            selectedCourse: 1,
+            courseList: [
+                {
+                    value: 1,
+                    label: "课程A"
+                },
+                {
+                    value: 2,
+                    label: "课程B"
+                }
+            ],
+            selectedClass: 1,
+            classList: [
+                {
+                    value: 1,
+                    label: "计1501"
+                },
+                {
+                    value: 2,
+                    label: "计1502"
                 }
             ]
         };
+    },
+    methods: {
+        addCourseToList(day, slot, courseName) {
+            var dayName = "";
+            switch (day) {
+                case 0:
+                    dayName = "mon";
+                    break;
+                case 1:
+                    dayName = "two";
+                    break;
+                case 2:
+                    dayName = "wed";
+                    break;
+                case 3:
+                    dayName = "thu";
+                    break;
+                case 4:
+                    dayName = "fri";
+                    break;
+                default:
+                    break;
+            }
+            this.awesomeCourseList[slot][dayName] = courseName;
+        },
+        fetchTableData() {
+            var apiUrl = "";
+            if (this.selectType === 0) apiUrl = "/course/timetable/teacher/";
+            else if (this.selectType === 1)
+                apiUrl = "/course/timetable/classroom/";
+            else if (this.selectType === 2) apiUrl = "/course/timetable/class/";
+            //cid rid tid
+            this.$Message.warning("查询中");
+        },
+        doArrange() {
+            this.$Message.warning("排课中");
+        }
+    },
+    beforeMount() {
+        this.isLoading = true;
+        let formData = new FormData();
+        formData.append("tid", 2);
+        this.$axios
+            .post("/course/timetable/teacher/", formData)
+            .then(response => {
+                this.course = response.data.data;
+                this.course.forEach(c => {
+                    var showStr =
+                        c.course.full_name +
+                        "(" +
+                        c.course.teacher +
+                        ")" +
+                        "(" +
+                        c.course.week_str +
+                        ")";
+                    this.addCourseToList(c.day, c.slot, showStr);
+                });
+                this.isLoading = false;
+            });
     }
 };
 </script>
+
+<style>
+.ivu-select {
+    margin: 10px 10px 10px 0;
+}
+</style>
+
--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
             "/api": {
                 target: "http://teach.test",
                 changeOrigin: true
+            },
+            "/course": {
+                target: "http://test.jingwei.site:8000",
+                changeOrigin: true
             }
         }
     }
